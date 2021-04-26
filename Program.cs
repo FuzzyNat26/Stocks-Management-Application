7ba64933de4f142d64fa3d8621020daae49d7003
Jonathan <jlee55261@gmail.com>
2021-04-26 16:50:01 +0700

    Update Error Message

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace Proyek_UAS
                 Application.Run(new Login());
             } catch (Exception e)
             {
-                MessageBox.Show(e.Message);
+                MessageBox.Show("Error Message " + e.Message + "." + Environment.NewLine + "Contact your app provider!");
             }
         }
     }
